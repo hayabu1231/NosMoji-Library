ecaa2a73e9998a00b8c88e4de45b911dfeff9861
御影　はやぶ <78709561+hayabu1231@users.noreply.github.com>
2025-01-02 02:09:35 +0900

    セットの追加機能を追加 (#1)
    * ログイン仕様の修正

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ window.addEventListener('error', (event) => {
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById('dialog-login').showModal();
     for (let i = 0; i < Pages.length; i++) {
         document.getElementById('open-' + Pages[i].id).addEventListener('click', function() {
             for (let k = 0; k < Pages.length; k++) {
@@ -42,6 +43,73 @@ document.addEventListener("DOMContentLoaded", (event) => {
         });
         document.getElementById('open-' + Pages[i].id).dataset.id = Pages[i].id;
     }
+    document.getElementById('myset-create').addEventListener('click', function() {
+        document.getElementById('dialog-create').show();
+    });
+    document.getElementById('dialog-login-btn').addEventListener('click', function() {
+        if (window.nostr) {
+            window.nostr.getPublicKey().then((pubkey) => {
+                Account.nip_07 = true;
+                Account.npub_converted = pubkey;
+                EMOJI.getUser();
+                document.getElementById('dialog-login').close();
+            });
+        } else {
+            addConsoleContent({status: 'error', message:'NIP-07の拡張機能が認識できませんでした。'});
+        }
+    });
+    document.getElementById('dialog-create-close').addEventListener('click', function() {
+        document.getElementById('dialog-create').close();
+    });
+    document.getElementById('dialog-create-add').addEventListener('click', function() {
+        if (Account.nip_07) {
+            var nowdate = Math.floor(new Date().getTime() / 1000);
+            var tags = [];
+            if (document.getElementById('dialog-create-id').value.length > 0) {
+                tags.push(['d', document.getElementById('dialog-create-id').value]);
+            }
+            if (document.getElementById('dialog-create-name').value.length > 0) {
+                tags.push(['title', document.getElementById('dialog-create-name').value]);
+            }
+            if (document.getElementById('dialog-create-icon-url').value.length > 0) {
+                tags.push(['image', document.getElementById('dialog-create-icon-url').value]);
+            }
+            if (document.getElementById('dialog-create-description').innerText.length > 0) {
+                tags.push(['description', document.getElementById('dialog-create-description').innerText]);
+            }
+            if (document.getElementById('dialog-create-emojis').children.length > 0) {
+                for (let i = 0; i < document.getElementById('dialog-create-emojis').children.length; i++) {
+                    tags.push(['emoji', document.getElementById('dialog-create-emojis').children[i].children[1].value, document.getElementById('dialog-create-emojis').children[i].children[0].value]);
+                }
+            } else {
+                addConsoleContent({status: 'error', message:'このクライアントでは絵文字なしで絵文字セットを作成することはできません。'});
+                return;
+            }
+            window.nostr.signEvent({
+                created_at: nowdate,
+                kind: 30030,
+                tags: tags,
+                content: ''
+            }).then(function (event) {
+                connection2.add({body: JSON.stringify(['EVENT', event])});
+            });
+        } else {
+            addConsoleContent({status: 'error', message:'NIP-07の拡張機能が認識できませんでした。'});
+        }
+    });
+    document.getElementById('dialog-create-icon-url').addEventListener('change', function() {
+        document.getElementById('dialog-create-icon').src = document.getElementById('dialog-create-icon-url').innerText;
+    });
+    document.getElementById('dialog-create-emojis-add').addEventListener('click', function() {
+        let block = createElement('div');
+        let url = createElement('input');
+        url.setAttribute('placeholder', '絵文字URL*');
+        block.append(url);
+        let code = createElement('input');
+        code.setAttribute('placeholder', 'ショートコード*');
+        block.append(code);
+        document.getElementById('dialog-create-emojis').append(block);
+    });
     document.getElementById('dialog-pack_info-add').addEventListener('click', function() {
         if (this.dataset.status == 'unadded') {
             EMOJI.install(this.dataset.id);
@@ -64,6 +132,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
     EMOJI.getAll();
 });
+setInterval(function() {
+    if (Account.need_users.length > 0){
+        connection2.add({
+            body: JSON.stringify([
+                'REQ',
+                `${Account.users_event_id}`,
+                {
+                    authors: Account.need_users,
+                    kinds: [0],
+                    limit: 20
+                }
+            ])
+        });
+    }
+}, 10000);
 
 function addConsoleContent(data) {
     document.getElementById('console').append(createElement('div', `${data.status}:${data.message}`));
@@ -113,7 +196,7 @@ function createPackElement(pack, list) {
         }
         document.getElementById('dialog-pack_info-icon').src = selected_pack.icon;
         document.getElementById('dialog-pack_info-name').innerText = selected_pack.name;
-        document.getElementById('dialog-pack_info-author').innerText = selected_pack.author.id;
+        document.getElementById('dialog-pack_info-author').innerText = selected_pack.author.name;
         document.getElementById('dialog-pack_info-description').innerText = selected_pack.description;
         let emoji_elements = [];
         for (let i = 0; i < selected_pack.emojis.length; i++) {
@@ -122,12 +205,53 @@ function createPackElement(pack, list) {
         document.getElementById('dialog-pack_info-emojis').replaceChildren(...emoji_elements);
         document.getElementById('dialog-pack_info').scroll(0,0);
         document.getElementById('dialog-pack_info').show();
-    }, `30030:${pack.name}:${pack.author.id}-${list}`);
-    element.dataset.id = `30030:${pack.name}:${pack.author.id}`;
+    }, `${pack.id}-${list}`);
+    element.dataset.id = pack.id;
     element.append(createElement('img', pack.icon, 'list-pack-icon'));
     element.append(createElement('p', pack.name, 'list-pack-name'));
-    element.append(createElement('small', pack.author.id, 'list-pack-author'));
-     return element;
+    element.append(createElement('small', pack.author.name, 'list-pack-author'));
+    return element;
+}
+
+function setEmojiPack(pack) {
+    EmojiPacks.set(pack.id, pack);
+    if (document.getElementById(`${pack.id}-new`)) {
+        document.getElementById(`${pack.id}-new`).replaceWith(createPackElement(pack, 'new'));
+    } else {
+        document.getElementById('pack-list-new').append(createPackElement(pack, 'new'));
+    }
+    if (
+        Account.npub_converted == pack.author.id &&
+        document.getElementById(`${pack.id}-mypack`)
+    ) {
+        document.getElementById(`${pack.id}-mypack`).replaceWith(createPackElement(pack, 'mypack'));
+    } else if (Account.npub_converted == pack.author.id) {
+        document.getElementById('pack-list-mypack').append(createPackElement(pack, 'mypack'));
+    }
+    if (
+        Account.following_emoji_packs.includes(pack.id) &&
+        document.getElementById(`${pack.id}-mylist`)
+    ) {
+        document.getElementById(`${pack.id}-mylist`).replaceWith(createPackElement(pack, 'mylist'));
+    } else if (Account.following_emoji_packs.includes(pack.id)) {
+        document.getElementById('pack-list-mylist').append(createPackElement(pack, 'mylist'));
+    }
+    
+    if (document.getElementById('pack-list-new').children.length == 0) {
+        document.getElementById('pack-list-new-status').innerText = 'このリストに対応するアイテムは0個です。';
+    } else {
+        document.getElementById('pack-list-new-status').innerText = '';
+    }
+    if (document.getElementById('pack-list-mypack').children.length == 0) {
+        document.getElementById('pack-list-mypack-status').innerText = 'このリストに対応するアイテムは0個です。';
+    } else {
+        document.getElementById('pack-list-mypack-status').innerText = '';
+    }
+    if (document.getElementById('pack-list-mylist').children.length == 0) {
+        document.getElementById('pack-list-mylist-status').innerText = 'このリストに対応するアイテムは0個です。';
+    } else {
+        document.getElementById('pack-list-mylist-status').innerText = '';
+    }
 }
 
 class Server {
@@ -149,16 +273,19 @@ class Server {
                 if (data[2].kind == 0) {
                     var user = USER.toHYB(data[2]);
                     if (Account.need_users.indexOf(user.id) > -1) {
-                        Account.need_users.splice(accounts.need_users.indexOf(user.id), 1);
+                        Account.need_users.splice(Account.need_users.indexOf(user.id), 1);
                     }
                 } else if (data[2].kind == 10030) {
                     var emojis = tagSearch(data[2].tags, 'a');
                     var emoji_authors = [];
                     var emoji_names = [];
+                    Account.following_emoji_packs = [];
                     if (emojis.length > 0) {
                         for (var i = 0; i < emojis.length; i++) {
                             Account.following_emoji_packs.push(emojis[i][1]);
-                            if (!EmojiPacks.has(emojis[i][1])) {
+                            if (EmojiPacks.has(emojis[i][1])) {
+                                setEmojiPack(EmojiPacks.get(emojis[i][1]));
+                            } else {
                                 emoji_authors.push(emojis[i][1].split(':')[1]);
                                 emoji_names.push(emojis[i][1].split(':')[2]);
                             }
@@ -167,7 +294,7 @@ class Server {
                     connection2.add({
                         body: JSON.stringify([
                             'REQ',
-                            `${accounts.emojis_event_id}`,
+                            `${Account.emojis_event_id}`,
                             {
                                 authors: emoji_authors,
                                 '#d': emoji_names,
@@ -178,22 +305,33 @@ class Server {
                     });
                 } else if (data[2].kind == 30030) {
                     let pack = {
+                        id: '',
                         icon: null,
                         name:  '未設定',
                         author: {
-                            id: data[2].pubkey
+                            id: data[2].pubkey,
+                            name: data[2].pubkey
                         },
                         description: 'このセットには説明文が用意されていません。',
                         emojis: []
                     };
+                    if (Users.has(pack.author.id)) {
+                        pack.author = Users.get(pack.author.id);
+                    } else {
+                        USER.get(pack.author.id);
+                    }
                     var d = tagSearch(data[2].tags, 'd');
                     if (d.length > 0) {
-                        pack.name = d[0][1];
+                        pack.id = d[0][1];
                     }
                     var titles = tagSearch(data[2].tags, 'title');
                     if (titles.length > 0) {
                         pack.name = titles[0][1];
                     }
+                    if (pack.name == '未設定') {
+                        pack.name = pack.id;
+                    }
+                    pack.id = `30030:${pack.author.id}:${pack.id}`;
                     var imageS = tagSearch(data[2].tags, 'image');
                     if (imageS.length > 0) {
                         pack.icon = imageS[0][1];
@@ -211,28 +349,7 @@ class Server {
                         if (!pack.icon) {
                             pack.icon = pack.emojis[0].url;
                         }
-                        EmojiPacks.set(`30030:${pack.name}:${pack.author.id}`, pack);
-                        if (document.getElementById(`30030:${pack.name}:${pack.author.id}-new`)) {
-                            document.getElementById(`30030:${pack.name}:${pack.author.id}-new`).replaceWith(createPackElement(pack, 'new'));
-                        } else {
-                            document.getElementById('pack-list-new').append(createPackElement(pack, 'new'));
-                        }
-                        if (
-                            Account.npub_converted == pack.author.id &&
-                            document.getElementById(`30030:${pack.name}:${pack.author.id}-myset`)
-                        ) {
-                            document.getElementById(`30030:${pack.name}:${pack.author.id}-myset`).replaceWith(createPackElement(pack, 'myset'));
-                        } else if (Account.npub_converted == pack.author.id) {
-                            document.getElementById('pack-list-myset').append(createPackElement(pack, 'myset'));
-                        }
-                        if (
-                            Account.following_emoji_packs.includes(`30030:${pack.name}:${pack.author.id}`) &&
-                            document.getElementById(`30030:${pack.name}:${pack.author.id}-mylist`)
-                        ) {
-                            document.getElementById(`30030:${pack.name}:${pack.author.id}-mylist`).replaceWith(createPackElement(pack, 'mylist'));
-                        } else if (Account.following_emoji_packs.includes(`30030:${pack.name}:${pack.author.id}`)) {
-                            document.getElementById('pack-list-mylist').append(createPackElement(pack, 'mylist'));
-                        }
+                        setEmojiPack(pack);
                     }
                 }
             } else if (data[0] == 'NOTICE') {
@@ -339,7 +456,10 @@ const EMOJI = {
     install(emoji) {
         if (Account.nip_07) {
             var nowdate = Math.floor(new Date().getTime() / 1000);
-            var tags = Account.following_emoji_packs;
+            var tags = [];
+            for (let i = 0; i < Account.following_emoji_packs.length; i++) {
+                tags.push(['a', Account.following_emoji_packs[i]]);
+            }
             tags.push(['a', emoji]);
             window.nostr.signEvent({
                 created_at: nowdate,
@@ -352,6 +472,63 @@ const EMOJI = {
         } else {
             addConsoleContent({status: 'error', message:'NIP-07の拡張機能が認識できませんでした。'});
         }
+    },
+    uninstall(emoji) {
+        if (Account.nip_07) {
+            var nowdate = Math.floor(new Date().getTime() / 1000);
+            var tags = [];
+            for (let i = 0; i < Account.following_emoji_packs.length; i++) {
+                if (emoji != Account.following_emoji_packs[i]) {
+                    tags.push(['a', Account.following_emoji_packs[i]]);
+                }
+            }
+            window.nostr.signEvent({
+                created_at: nowdate,
+                kind: 10030,
+                tags: tags,
+                content: ''
+            }).then(function (event) {
+                connection2.add({body: JSON.stringify(['EVENT', event])});
+            });
+        } else {
+            addConsoleContent({status: 'error', message:'NIP-07の拡張機能が認識できませんでした。'});
+        }
+    }
+};
+
+const USER = {
+    toHYB: function(old_user) {
+        old_user.content = JSON.parse(old_user.content);
+        var new_user = {
+            id: old_user.pubkey,
+            name: old_user.content.name,
+            avatar: old_user.content.picture,
+            timestamp: old_user.created_at * 1000
+        };
+        if (!new_user.name) {
+            new_user.name = `${old_user.pubkey.slice(0, 7)}…`;
+        }
+        if (
+            Users.has(new_user.id) &&
+            Users.get(new_user.id).timestamp &&
+            new Date(Users.get(new_user.id).timestamp) > new Date(new_user.timestamp)
+        ) {
+            new_user = Users.get(new_user.id);
+        }
+        Users.set(new_user.id, new_user);
+        EmojiPacks.forEach((pack) => {
+            if (pack.author.id == new_user.id) {
+                pack.author = new_user;
+                setEmojiPack(pack);
+            }
+        });
+        return new_user;
+    },
+    get(id) {
+         if (!Account.need_users.includes(id)) {
+            Account.need_users.push(id);
+         }
+         return true;
     }
 };
 
